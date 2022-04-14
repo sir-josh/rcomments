1192fa81d034300d3e13625b9f1ec945f6b5fc40
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-14 10:00:27 +0100

    set up native comment page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from "react-dom";
 
 const App = () => {
     return(
-        <div> Hi there!</div>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">Sam</a>
+                    <div className="metadata">
+                        <span className="date"> Today at 6pm</span>
+                    </div>
+                    <div className="Nice blog">Nice blog post</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
